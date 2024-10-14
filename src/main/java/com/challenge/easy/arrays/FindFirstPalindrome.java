8f7defa8178d227689fa5804c8dd08569025a2f7
Luciano <dosthin.luciano@accenture.com>
2024-10-14 15:56:36 -0600

    FindFirstPalindrome solved

--- src/main/java/com/challenge/easy/arrays/FindFirstPalindrome.java
+++ src/main/java/com/challenge/easy/arrays/FindFirstPalindrome.java
@@ -30,7 +30,26 @@ package com.challenge.easy.arrays;
 public class FindFirstPalindrome {
 
     public static String firstPalindrome(String[] words) {
-        return null;
+        for(String word : words) {
+            if(isPalindrome(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+
+    public static boolean isPalindrome(String word) {
+        int left = 0;
+        int right = word.length() -1;
+
+        while (left < right) {
+            if(word.charAt(left) != word.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;   
+        }
+        return true;
     }
 
     public static void main(String[] args) {
