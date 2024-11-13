54296086bf1159d5ccf40d1293e900e7189478df
Carlos Cruz <jcrodgz@gmail.com>
2024-11-13 14:13:27 -0600

    Finishing code

--- src/main/java/com/challenge/easy/dfs/TreeInorderTraversal.java
+++ src/main/java/com/challenge/easy/dfs/TreeInorderTraversal.java
@@ -1,44 +1,54 @@
 package com.challenge.easy.dfs;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
-    # Binary Tree Inorder Traversal
+ * # Binary Tree Inorder Traversal
+ *
+ *  *Given the root of a binary tree, return the inorder traversal of its nodes'
+ * values.
+ *
+ *  *Example 1:
+ *
+ *  *1 \ 2 / 3
+ *
+ *  *https://assets.leetcode.com/uploads/2020/09/15/inorder_1.jpg
+ *
+ *  *Input: root = [1,null,2,3] Output: [1,3,2]
+ *
+ *  *Example 2: Input: root = [] Output: []
+ *
+ *  *Example 3: Input: root = [1] Output: [1]
+ *
+ *  *Constraints:
 
-     Given the root of a binary tree, return the inorder traversal of its nodes' values.
-
-     Example 1:
-
-         1
-          \
-           2
-          /
-         3
-
-         https://assets.leetcode.com/uploads/2020/09/15/inorder_1.jpg
-
-         Input: root = [1,null,2,3]
-         Output: [1,3,2]
-
-     Example 2:
-         Input: root = []
-         Output: []
-
-     Example 3:
-         Input: root = [1]
-         Output: [1]
-
-     Constraints:
-
-         * The number of nodes in the tree is in the range [0, 100].
-         * -100 <= Node.val <= 100
-
-     Follow up: Recursive solution is trivial, could you do it iteratively?
+  **** The number of nodes in the tree is in the range [0, 100]. -100 <= Node.val
+ * <= 100
+ *
+ *  *Follow up: Recursive solution is trivial, could you do it iteratively?
  */
 public class TreeInorderTraversal {
 
     public List<Integer> inorderTraversal(TreeNode root) {
-        return null;
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            result.add(current.val);
+
+            current = current.right;
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
@@ -46,6 +56,7 @@ public class TreeInorderTraversal {
     }
 
     public class TreeNode {
+
         int val;
         TreeNode left;
         TreeNode right;
@@ -65,5 +76,3 @@ public class TreeInorderTraversal {
         }
     }
 }
-
-
